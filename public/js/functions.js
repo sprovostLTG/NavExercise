6b2f5bb452ef0c826e8749d6c50fa4611de17a82
Shane Provost <shane.provost@yahoo.com>
2015-02-19 20:15:16 -0600

    Styling of desktop nav, structure for header panel, transition mixins

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,25 +3,36 @@ $(document).ready(function(){
 		
 		var lis = info;
 		var output = '';
-
+		output += '<a href="/"><img id="dLogo" src="images/huge-white.png" /></a>';
 		output += '<ul>';
-		
+		output += 	'<li class="logo"></li>';
 		for(var i = 0; i < lis.items.length; i++){
-			var item = lis.items[i];
-			output += '<li><a href="' + item.url + '">' + item.label + '</a></li>';
-
-			if( item.items.length ){
+			var topNav = lis.items[i];
+			output += '<li><a href="' + topNav.url + '">'; 
+			output += topNav.label + '</a>';
+			if( topNav.items.length ){
 				output += '<ul class="sub">';
-				for(var i = 0; i < item.items.length; i++){
-					var subli = item.items[i];
+				for(var j = 0; j < topNav.items.length; j++){
+					var subli = topNav.items[j];
 					output += '<li><a href="' + subli.url + '">' + subli.label + '</a></li>';
 				}
 				output += '</ul>';
 			}
+			output += '</li>';
+
+			// if( item.items.length ){
+			// 	output += '<ul class="sub">';
+			// 	for(var i = 0; i < item.items.length; i++){
+			// 		var subli = item.items[i];
+			// 		output += '<li><a href="' + subli.url + '">' + subli.label + '</a></li>';
+			// 	}
+			// 	output += '</ul>';
+			// }
 		}
 
 		output += '</ul>';
 
 		$('#nav').html(output);
+		$('#resNav').html(output);
 	});
 });
