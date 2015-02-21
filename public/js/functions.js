06f179fd3debff4a18996424a41a52be6a724bbd
Shane Provost <shane.provost@yahoo.com>
2015-02-21 06:54:27 -0600

    click logic to handle menu actions (responsive and desktop), body styling

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
 		
 		var lis = info;
 		var output = '';
-		output += '<a href="/"><img id="dLogo" src="images/huge-white.png" /></a>';
+		output += '<a href="/"><img class="logoPull" id="dLogo" src="images/huge-white.png" /></a>';
 		output += '<ul>';
 		for(var i = 0; i < lis.items.length; i++){
 			var topNav = lis.items[i];
-			output += '<li><a href="' + topNav.url + '">'; 
+			output += '<li><a class="prime" href="' + topNav.url + '">'; 
 			output += topNav.label + '</a>';
 			if( topNav.items.length ){
-				output += '<div class="resSubDisp" href="#"><i class="fa fa-chevron-down"></i></div>';
+				output += '<a class="resSubDisp" href="#"><i class="fa fa-chevron-down"></i></a>';
 				output += '<ul class="sub">';
 				for(var j = 0; j < topNav.items.length; j++){
 					var subli = topNav.items[j];
@@ -27,15 +27,58 @@ $(document).ready(function(){
 
 		$('#nav').html(output);
 		$('#resNav').html(output);
-		$('#resNav').append('<div class="legal">&copy; 2015 Huge. All Rights Reserved.</div>');
+		$('#resNav').append('<div id="legal">&copy; 2015 Huge. All Rights Reserved.</div>');
+		$('#legal').addClass('abs');
+
+		function menuLinks(){
+			if($(window).width() > 769){
+				$("#nav li:has(ul)").hover(function () {
+				    $(this).children(".prime").click(function () {
+				    	$('.sub').removeClass('open');
+				    	$(this).siblings('ul').addClass('open');
+				       	event.preventDefault();
+				    });
+				});
+			}	
+		}
+
+		menuLinks();
+
+		$(window).resize(function(){
+			menuLinks();
+		});		
+
+		//check height of responsive menu and handle the legal copy
+		function resMenuHeightCheck(){
+			var navHeight = $('#resNav ul').height();
+			var currentWinHeight = $(window).height() - 72;
+			//alert('navigation: ' + navHeight + '. window: ' + currentWinHeight);
+			if (winHeight > navHeight){
+				$('#legal').addClass('rel');
+				$('#legal').removeClass('abs');
+			} else {
+				$('#legal').addClass('abs');
+				$('#legal').removeClass('rel');
+			}
+		}
+
+		//flip the chevron on click
+		$('.resSubDisp').on('click', function(){
+
+			if($(this).hasClass('flip')){
+				$(this).removeClass('flip');
+			} else {
+				$(this).addClass('flip');
+			}
+			$(this).siblings('ul').slideToggle('fast', resMenuHeightCheck() );
+			
+		});
 
 	});
 
-	$('.resSubDisp').on('click', function(){
-		alert('hi!');
-	});
 
 	//Handle responsive menu height
+
 	var winHeight = $(window).height();
 
 	$('#resNav').css('height', winHeight - 72 + 'px');
@@ -45,8 +88,7 @@ $(document).ready(function(){
 		$('#resNav').css('height', winHeight - 72 + 'px');
 	});
 
-	//handle the menu toggling
-	$('.menuToggle').click(function(){
+	function handleNav(){
 		var winWidth = $(window).width() - 72;
 		$this = $(this);
 
@@ -56,21 +98,58 @@ $(document).ready(function(){
 			$('#resNav').addClass('negMarg');
 		}
 
-		if($this.hasClass('open')){
-			$this.removeClass('open');
-			$this.children('#open').removeAttr('style', '-webkit-transform:translateX('+ winWidth +'px);');
-			$this.children('#open').removeAttr('style', 'transform:translateX('+ winWidth +'px);');
-			$this.children('#close').removeAttr('style', '-webkit-transform:translateX('+ winWidth +'px);');
-			$this.children('#close').removeAttr('style', 'transform:translateX('+ winWidth +'px);');
+		if( $('#dLogo').hasClass('logoPull') ){
+			$('#dLogo').removeClass('logoPull');
 		} else {
-			$this.addClass('open');
-			$this.children('#open').attr('style', '-webkit-transform:translateX('+ winWidth +'px);');
-			$this.children('#open').attr('style', 'transform:translateX('+ winWidth +'px);');
-			$this.children('#close').attr('style', '-webkit-transform:translateX('+ winWidth +'px);');
-			$this.children('#close').attr('style', 'transform:translateX('+ winWidth +'px);');
+			$('#dLogo').addClass('logoPull');
 		}
 
+		if($('.menuToggle').hasClass('open')){
+			$('.menuToggle').removeClass('open');
+			$('.menuToggle').children('#open').removeAttr('style', '-webkit-transform:translateX('+ winWidth +'px);');
+			$('.menuToggle').children('#open').removeAttr('style', 'transform:translateX('+ winWidth +'px);');
+			$('.menuToggle').children('#close').removeAttr('style', '-webkit-transform:translateX('+ winWidth +'px);');
+			$('.menuToggle').children('#close').removeAttr('style', 'transform:translateX('+ winWidth +'px);');
+		} else {
+			$('.menuToggle').addClass('open');
+			$('.menuToggle').children('#open').attr('style', '-webkit-transform:translateX('+ winWidth +'px);');
+			$('.menuToggle').children('#open').attr('style', 'transform:translateX('+ winWidth +'px);');
+			$('.menuToggle').children('#close').attr('style', '-webkit-transform:translateX('+ winWidth +'px);');
+			$('.menuToggle').children('#close').attr('style', 'transform:translateX('+ winWidth +'px);');
+			$('.mask').fadeIn(300);
+		}
+
+		//shift body over if responsive
+		if($(window).width() < 769){
+			if($('.page').hasClass('bodyMove')){
+				$('.page').removeClass('bodyMove');
+			} else {
+				$('.page').addClass('bodyMove');
+			}
+		}
+	} // end handleNav()
+
+	//handle the menu toggling
+	$('.menuToggle').click(function(){
+		handleNav();
+
 		return false;
+	});
+
+
+	//close the menu and move body back if page is clicked
+	$('.page').click(function(){
+		$('.page').removeClass('bodyMove');
+		$('#resNav').addClass('negMarg');
+		$('#dLogo').addClass('logoPull');
+		$('.menuToggle').removeClass('open');
+		$('.menuToggle').children('#open').removeAttr('style', '-webkit-transform:translateX('+ winWidth +'px);');
+		$('.menuToggle').children('#open').removeAttr('style', 'transform:translateX('+ winWidth +'px);');
+		$('.menuToggle').children('#close').removeAttr('style', '-webkit-transform:translateX('+ winWidth +'px);');
+		$('.menuToggle').children('#close').removeAttr('style', 'transform:translateX('+ winWidth +'px);');
+		$('.sub').removeClass('open');
+		$('.mask').fadeOut(300);
+
 	});
 
 
